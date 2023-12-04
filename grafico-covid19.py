ead1e09b30948504d2f9df7da484205a85ac9f72
Marina Mattos <107765135+marinamattos@users.noreply.github.com>
2023-12-04 11:47:04 -0300

    Add files via upload

---
+++ grafico-covid19.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Criando gráfico interativo Covid 19
+# 
+# Projeto para criação de gráficos interativos para análise da evolução do Covid 19.Os dados foram obtidos a partir do site: https://github.com/wcota/covid19br
+
+# In[26]:
+
+
+#Bibliotecas
+
+import pandas as pd
+import plotly.express as px
+import streamlit as st 
+
+
+# In[30]:
+
+
+#Baixando do arquivo
+df = pd.read_csv('cases-brazil-states.csv')
+
+#Renomenado colunas
+df = df.rename(columns={'newDeaths': 'Novos óbitos','newCases': 'Novos casos','deaths_per_100k_inhabitants': 'Óbitos por 100 mil habitantes','totalCases_per_100k_inhabitants':'Casos por 100 mil habitantes'})
+
+#Selecionando estados 
+estados = list(df['state'].unique())
+state = st.selectbox('Qual estado?', estados)
+
+#Selecionando colunas 
+colunas = ['Novos óbitos','Novos casos','Óbitos por 100 mil habitantes','Casos por 100 mil habitantes']
+column = st.sidebar.selectbox('Qual tipo de informação?', colunas)
+
+#Seleção das linhas que pertencem ao estado 
+df = df[df['state'] == state]
+
+#Plotando nos gráficos
+fig = px.line(df, x="date", y=column, title=column + ' - ' + state)
+fig.update_layout( xaxis_title='Data', yaxis_title=column.upper(), title = {'x':0.5})
+
+st.title('DADOS COVID - BRASIL')
+st.write('Nessa aplicação, o usuário tem a opção de escolher o estado e o tipo de informação para mostrar o gráfico. Utilize o menu lateral para alterar a mostragem.')
+
+st.plotly_chart(fig, use_container_width=True)
+
+st.caption('Os dados foram obtidos a partir do site: https://github.com/wcota/covid19br')
+
+
+# In[ ]:
+
+
+
+
